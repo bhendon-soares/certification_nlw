4b2fab8a6eb800ab95e0f65aa136c6351e3ced7a
Bhendon Soares <bhendon.soares12@gmail.com>
2024-02-06 20:53:43 -0300

    feat: adiciona controller de estudante e DTO de verificação

---
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
@@ -0,0 +1,19 @@
+package com.rocketseat.certification_nlw.modules.students.controllers;
+
+import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @PostMapping("/verifyIfHasCertification")
+    public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertificationDTO){
+
+        System.out.println(verifyHasCertificationDTO);
+        return "Usuário pode fazer a prova";
+    }
+}
